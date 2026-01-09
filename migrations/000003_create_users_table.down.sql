f893035b0a54ea8cdc252899931930437e7ac704
nuu-maan <n82238895@gmail.com>
2026-01-09 18:13:48 +0530

    Create users table migration with role enum and email verification

---
+++ migrations/000003_create_users_table.down.sql
@@ -0,0 +1,11 @@
+DROP TRIGGER IF EXISTS update_users_updated_at ON users;
+DROP FUNCTION IF EXISTS update_updated_at_column();
+DROP INDEX IF EXISTS idx_users_deleted_at;
+DROP INDEX IF EXISTS idx_users_oauth_provider;
+DROP INDEX IF EXISTS idx_users_role;
+DROP INDEX IF EXISTS idx_users_password_reset_token;
+DROP INDEX IF EXISTS idx_users_email_verification_token;
+DROP INDEX IF EXISTS idx_users_email;
+DROP INDEX IF EXISTS idx_users_username;
+DROP TABLE IF EXISTS users;
+DROP TYPE IF EXISTS user_role;
