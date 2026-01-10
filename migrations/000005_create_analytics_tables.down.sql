2b94d6e5555d17a405c1b3a8877a8c7cde16b5fc
nuu-maan <n82238895@gmail.com>
2026-01-10 23:29:37 +0530

    feat(migrations): create video_views table for analytics tracking

---
+++ migrations/000005_create_analytics_tables.down.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_video_views_device_type;
+DROP INDEX IF EXISTS idx_video_views_country;
+DROP INDEX IF EXISTS idx_video_views_created_at;
+DROP INDEX IF EXISTS idx_video_views_user_id;
+DROP INDEX IF EXISTS idx_video_views_video_id;
+DROP TABLE IF EXISTS video_views;
